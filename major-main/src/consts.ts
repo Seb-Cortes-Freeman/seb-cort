ab7132c275702ad76c7648e18c67af8eda7d9b73
Seb-Cortes-Freeman <cortesfreemansebastian@gmail.com>
2026-06-01 18:26:58 +0100

    Convert major-main from sobmodule to regular directory

---
+++ major-main/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Geopolitics & Public Policy';
+export const SITE_DESCRIPTION = 'Writing and interactive data visualizations on geopolitics and public policy.';
